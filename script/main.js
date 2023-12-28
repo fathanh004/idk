4244c1495a2bf22bc94ed62c2ec02b6f3d5691d5
Fathan Hidayatullah <80765419+fathanh004@users.noreply.github.com>
2023-12-29 00:29:07 +0700

    Update main.js

--- script/main.js
+++ script/main.js
@@ -293,7 +293,7 @@ const animationTimeline = () => {
     );
 
   // tl.seek("currentStep");
-  tl.timeScale(1);
+  tl.timeScale(0.5);
 
   // Restart Animation on click
   const replyBtn = document.getElementById("replay");
